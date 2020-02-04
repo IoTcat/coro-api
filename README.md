75612f6474a106a78187173efbbe2cf0d4da4ed6
iotcat <i@iotcat.me>
2020-02-04 12:09:07 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # coro-api
 新型冠状病毒 api
+
+## 快速开始
+
+### 环境要求
+ - `nodeJS`支持`express`任意版本
+ - `git`任意版本
+
+### 下载文件
+```
+$ git clone https://github.com/iotcat/coro-api.git & cd coro-api
+```
+
+### 安装依赖
+```shell
+$ npm install
+```
+
+### 启动
+```shell
+$ node index.js
+```
+
+## 访问
+```
+http://localhost:17676
+```
+
+## GET参数
+ 参数 | 描述 | 示例 
+ :--: | :--: | :--:
+ province | 省份(国家) | 山东
+ city | 城市 | 泰安
+
+```
+http://localhost:17676/?province=山东
+```
+```
+http://localhost:17676/?city=泰安
+```
+
+## 进阶
+
+### 修改端口
+
+从`index.js`第9行修改默认端口。
+
+### 使用pm2守护进程
+
+```shell
+$ npm i -g pm2
+$ pm2 start index.js --name coro-api -o /var/log/coro/api.out -e /var/log/coro/api.err --watch
+```
+
+### Nginx配置
+
+将以下内容加入`server{}`中  
+```nginx
+location / {
+    proxy_pass   http://127.0.0.1:17676;
+}
+
+```
+如果你不是从根目录映射，记得去`index.js`中第11行`app.get('/', f)`修改成你的路径。
+
+### 爬虫配置
+
+爬虫使用[coro-py](https://github.com/iotcat/coro-py), 请参考其文档。
+
+## 开源协议
+
+本项目使用MIT协议，允许非署名商业非商业使用。武汉加油！中国加油！！❤
---
+++ index.js
@@ -0,0 +1,77 @@
+/*
+ * @proj: coro-api
+ * @Author: IoTcat (https://iotcat.me) 
+ * @Date: 2020-02-04 11:34:32 
+ */
+const coro = require('coro-py');
+var app = require('express')();
+
+app.listen(17676 /*default port*/, () => console.log('Coro API listening on port 17676!'));
+
+app.get('/', async (req, res) => {
+	var o = await coro();
+
+	/* req province & city */
+	if(req.query.hasOwnProperty('province')){
+		/* reg province */
+		o =	await (async () => new Promise(resolve => {
+			o.forEach((item, index) => {
+				if(item.provinceName == req.query.province || item.provinceShortName == req.query.province){
+					resolve(item);
+				}else{
+					if(index == o.length-1){
+						resolve({});
+					}
+				}
+			});
+		}))();
+		/* reg city */
+		if(req.query.hasOwnProperty('city') && o.hasOwnProperty('cities')){
+			o = await (async () => new Promise(resolve => {
+				o.cities.forEach((item, index) => {
+					if(item.cityName == req.query.city){
+						resolve(item);
+					}else{
+						if(index == o.length-1){
+							resolve({});
+						}
+					}
+				});
+			}))();
+		}
+
+		res.send(o);
+		return;
+	}
+
+	/* req city */
+	if(req.query.hasOwnProperty('city')){
+		o =	await (async () => new Promise(resolve => {
+			o.forEach((item, index) => {
+				if(item.hasOwnProperty('cities')){
+
+					item.cities.forEach((city, city_index) => {
+						if(city.cityName == req.query.city){
+							resolve(city);
+						}else{
+							if(city_index == item.cities.length-1){
+								resolve({});
+							}
+						}
+					});
+				}else{
+					if(index == o.length-1){
+						resolve({});
+					}
+				}
+			});
+		}))();
+
+		res.send(o);
+		return;
+	}
+
+	/* other */
+	res.send(o);
+
+});
